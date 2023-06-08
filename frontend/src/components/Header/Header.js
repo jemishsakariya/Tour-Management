83a0afba4d2f95ea2eea6afcb06fe657515a4f43
Jemish sakariya <74782154+jemishsakariya@users.noreply.github.com>
2023-06-08 15:56:47 +0530

    header nav responsive
    header nav responsive and App ,search-bar responsive

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -23,6 +23,8 @@ const nav__links = [
 
 const Header = () => {
   const headerRef = useRef(null);
+
+  const menuRef = useRef(null);
   //
   const navigate = useNavigate();
   const { user, dispatch } = useContext(AuthContext);
@@ -50,6 +52,10 @@ const Header = () => {
     return window.removeEventListener("scroll", stickyHeaderFunc);
   });
 
+  const toggleMenu = () => {
+    menuRef.current.classList.toggle("show__menu");
+  };
+
   return (
     <>
       <header className="header" ref={headerRef}>
@@ -62,7 +68,7 @@ const Header = () => {
               </div>
 
               {/* menu */}
-              <div className="navigation">
+              <div className="navigation" ref={menuRef} onClick={toggleMenu}>
                 <ul className="menu d-flex align-items-center gap-5">
                   {nav__links.map((item, index) => {
                     return (
@@ -103,7 +109,7 @@ const Header = () => {
                   )}
                 </div>
 
-                <span className="mobile__menu">
+                <span className="mobile__menu" onClick={toggleMenu}>
                   <i className="ri-menu-line"></i>
                 </span>
               </div>
