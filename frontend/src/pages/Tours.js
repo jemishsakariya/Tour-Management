5064737862015cb6b144e32220a88e44fa22042b
Jemish sakariya <74782154+jemishsakariya@users.noreply.github.com>
2023-06-08 16:30:48 +0530

    finish

--- frontend/src/pages/Tours.js
+++ frontend/src/pages/Tours.js
@@ -54,7 +54,7 @@ const Tours = () => {
             <Row>
               {tours?.map((tour) => {
                 return (
-                  <Col lg="3" key={tour._id}>
+                  <Col lg="3" md="6" sm="6" key={tour._id}>
                     <TourCard tour={tour} />
                   </Col>
                 );
