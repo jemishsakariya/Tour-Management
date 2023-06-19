00f2967f197e1ecc8d5f4c9ee6bdc1e4f59c26d4
Jemish sakariya <74782154+jemishsakariya@users.noreply.github.com>
2023-06-19 16:24:22 +0530

    Removed folder from repository

--- deploy/routes/reviewsRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { createReview } = require("../controllers/reviewController");
-const { verifyUser } = require("../utils/verifyToken");
-
-router.post("/:tourId", verifyUser, createReview);
-
-module.exports = router;
